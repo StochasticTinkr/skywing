c4b3e84314ad74d90b32a8b658636fdb96e0df3c
Daniel Pitts <git@stochastictinker.com>
2023-01-23 08:26:59 -0800

    Add some more utility.

--- src/main/kotlin/com/stochastictinkr/skywing/awt/_Container.kt
+++ src/main/kotlin/com/stochastictinkr/skywing/awt/_Container.kt
@@ -3,5 +3,5 @@ package com.stochastictinkr.skywing.awt
 import java.awt.Component
 import java.awt.Container
 
-fun <C : Component> Container.addComponent(constraints: Any? = null, component: C, init: C.() -> Unit = {}) =
-    component.also(init).also { add(it, constraints) }
+fun <C : Component> Container.addComponent(constraints: Any? = null, component: C, init: C.() -> Unit = {}): C =
+    component.apply(init).also { add(it, constraints) }
--- src/main/kotlin/com/stochastictinkr/skywing/rendering/PaintContext.kt
+++ src/main/kotlin/com/stochastictinkr/skywing/rendering/PaintContext.kt
@@ -1,13 +1,17 @@
 package com.stochastictinkr.skywing.rendering
 
+import com.stochastictinkr.skywing.awt.geom.Angle
 import com.stochastictinkr.skywing.hints.Hints
 import com.stochastictinkr.skywing.rendering.geom.by
 import java.awt.BasicStroke
+import java.awt.Color
 import java.awt.Font
 import java.awt.FontMetrics
 import java.awt.Graphics2D
 import java.awt.Paint
+import java.awt.Rectangle
 import java.awt.Shape
+import java.awt.geom.Point2D
 
 @DslMarker
 annotation class PaintDsl
@@ -15,11 +19,15 @@ annotation class PaintDsl
 data class PaintContext(val g: Graphics2D, val width: Int, val height: Int) {
     val size = width by height
     var paint: Paint? by g::paint
-
+    var color: Color? by g::color
     val hints = Hints(g)
+    val clipBounds: Rectangle by g::clipBounds
 
     var font: Font? by g::font
     val fontMetrics: FontMetrics? by g::fontMetrics
+
+    fun fontMetrics(font: Font) = g.getFontMetrics(font)
+
     fun stroke(init: StrokeBuilder.() -> Unit) {
         g.stroke = StrokeBuilder().apply(init).asBasicStroke()
     }
@@ -54,8 +62,15 @@ data class PaintContext(val g: Graphics2D, val width: Int, val height: Int) {
 
     fun fill(s: Shape) = g.fill(s)
 
-    fun hints(config: Hints.() -> Unit) {
-        hints.config()
-    }
+    fun hints(config: Hints.() -> Unit) = hints.config()
+
+    fun translate(tx: Double, ty: Double) = g.translate(tx, ty)
+    fun translate(vect: Point2D) = g.translate(vect.x, vect.y)
+    fun rotate(angle: Angle) = g.rotate(angle.radians)
+    infix fun Angle.rotateAround(point: Point2D) = g.rotate(radians, point.x, point.y)
+    fun rotate(angle: Angle, point: Point2D) = angle rotateAround point
+    fun rotate(angle: Angle, tx: Double, ty: Double) = g.rotate(angle.radians, tx, ty)
+    fun scale(sx: Double, sy: Double) = g.scale(sx, sy)
+    fun shear(shx: Double, shy: Double) = g.shear(shx, shy)
 }
 
