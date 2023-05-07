f859239e33bf9fdd0622f89e4d72f933aff95d7c
Daniel Pitts <git@stochastictinker.com>
2023-05-07 08:09:11 -0700

    New `addAction` method to add an action to a JComponent.

--- src/main/kotlin/com/stochastictinkr/skywing/swing/_JComponent.kt
+++ src/main/kotlin/com/stochastictinkr/skywing/swing/_JComponent.kt
@@ -1,5 +1,6 @@
 package com.stochastictinkr.skywing.swing
 
+import javax.swing.Action
 import javax.swing.JComponent
 import javax.swing.JPopupMenu
 import javax.swing.border.Border
@@ -10,4 +11,12 @@ inline fun <T : Border> JComponent.border(build: BorderFactory.() -> T): T {
 
 fun JComponent.popupMenu(init: JPopupMenu.() -> Unit) {
     this.componentPopupMenu = makePopupMenu(init)
+}
+
+/**
+ * Adds an accelerated action to the inputMap and actionMap of this JComponent
+ */
+fun JComponent.addAction(action: Action) {
+    inputMap.put(action.accelerator, action.actionCommand)
+    actionMap.put(action.actionCommand, action)
 }
