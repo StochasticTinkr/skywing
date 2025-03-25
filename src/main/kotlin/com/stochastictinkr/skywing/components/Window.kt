7f25585714371eb49f0cb88dac9799a68312a955
Daniel Pitts <git@stochastictinker.com>
2025-03-25 08:08:47 -0700

    Add some KDoc to the Window extensions.

--- src/main/kotlin/com/stochastictinkr/skywing/components/Window.kt
+++ src/main/kotlin/com/stochastictinkr/skywing/components/Window.kt
@@ -1,10 +1,20 @@
 package com.stochastictinkr.skywing.components
 
-import com.stochastictinkr.skywing.utils.Disposable
-import java.awt.Window
-import java.awt.event.WindowAdapter
-import java.awt.event.WindowEvent
+import com.stochastictinkr.skywing.utils.*
+import java.awt.*
+import java.awt.event.*
 
+/**
+ * Whether the window is in full screen mode.  This is determined by the [GraphicsDevice.fullScreenWindow] property.
+ *
+ * When setting this property, there are three possible outcomes:
+ *
+ * | New Value | Condition                          | Result                                                             |
+ * |-----------|------------------------------------|--------------------------------------------------------------------|
+ * | `true`    | Any                                | The window will be made fullscreen on its current [GraphicsDevice] |
+ * | `false`   | The window is currently fullscreen | The window will be made not fullscreen                             |
+ * | `false`   | The window is not fullscreen       | No operation                                                       |
+ */
 var Window.isFullScreen: Boolean
     get() =
         graphicsConfiguration.device.fullScreenWindow == this
@@ -16,6 +26,9 @@ var Window.isFullScreen: Boolean
         }
     }
 
+/**
+ * Attach a listener that will be called when the window is closed.
+ */
 inline fun Window.onWindowClosed(crossinline block: (WindowEvent) -> Unit): Disposable {
     val listener = object : WindowAdapter() {
         override fun windowClosed(e: WindowEvent) {
@@ -26,6 +39,9 @@ inline fun Window.onWindowClosed(crossinline block: (WindowEvent) -> Unit): Disp
     return Disposable { removeWindowListener(listener) }
 }
 
+/**
+ * Configure the window and make it visible.
+ */
 fun <C : Window> C.configureAndShow(configure: C.() -> Unit) = apply {
     configure()
     isVisible = true
